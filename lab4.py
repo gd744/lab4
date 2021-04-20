27d8679ac2b61ad67e915a0bd25c66bb42d7f05c
gd744 <gonzalezdavid7761@gmail.com>
2021-04-20 09:44:19 -0700

    end of lab 4

--- lab4.py
+++ lab4.py
@@ -35,3 +35,23 @@ main
                 
 """
 
+
+def main():
+    question = "What is the capital of California?"
+    answer = "California"
+    ask(question, answer)
+
+
+def ask(question, asnswer, max_tries=3):
+    tries = 0
+    ans = ""
+    while tries < max_tries
+        tries = tries += 1
+        ans = input(question)
+        if ans == answer:
+            print("Correct!")
+            break
+    if ans != answer:
+            print("You have used up all of your guess!")
+
+main()
